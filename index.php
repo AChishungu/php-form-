7d5e342bdf85503b1465c9ee22207404eaa0db3d
AChishungu <72464199+AChishungu@users.noreply.github.com>
2020-11-19 11:32:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-  echo '<p> this is php1</p.
+  //echo '<p> this is php1</p.
+if($_GET["firstname"]) {//show feedback
+}else{//show from
+  echo'<form action="">
+  first name <input type="text" name="firstname"/>
+  <br/.
+  <input type="submat">
+  
+  </form>
+
+
  ?>
  <p> this html </p>
